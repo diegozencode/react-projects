60766458030402f86de22685f02d6feff22ba644
Diego <difemo91@gmail.com>
2021-08-24 14:18:31 -0500

    [FEAT - ECOSYSTEM] add todos components

--- linkedin-learning/react-ecosystem/src/App.js
+++ linkedin-learning/react-ecosystem/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { hot } from 'react-hot-loader';
+import TodoList from './todos/TodoList';
 import './App.css';
 
 // function component
 const App = () => {
   return <div className="App">
     <h1>Hello, World!!!</h1>
+    <TodoList />
   </div>
 };
 
