18f884513b9d22d3154182850d1e9266012cc67c
Diego <difemo91@gmail.com>
2021-09-01 15:56:43 -0500

    [FEAT - JSX] add initial files

---
+++ digital-ocean/jsx-tutorial/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <h1 id="greeting">Hello world</h1>
+      <p>I am writting JSX</p>
+    </div>
+  );
+}
+
+export default App;
