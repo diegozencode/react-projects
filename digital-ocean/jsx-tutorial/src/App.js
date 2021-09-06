0ce296b375f2278a1003e7efe1d74193459dcb89
Diego <difemo91@gmail.com>
2021-09-06 18:51:55 -0500

    [FEAT - JSX] add custom application with JSX

--- digital-ocean/jsx-tutorial/src/App.js
+++ digital-ocean/jsx-tutorial/src/App.js
@@ -1,10 +1,40 @@
 import './App.css';
 
+const displayEmojiName = event => alert(event.target.id);
+// Aray of objects
+const emojis = [
+  {
+    emoji: "😀",
+    name: "grinning face",
+  },
+  {
+    emoji: "🎉",
+    name: "party popper",
+  },
+  {
+    emoji: "💃",
+    name: "woman dancing",
+  },
+];
+
 function App() {
+  const greeting = "greeting";
+  const displayAction = true;
   return (
     <div className="container">
-      <h1 id="greeting">Hello world</h1>
-      <p>I am writting JSX</p>
+      <h1 id={greeting}>Hello world</h1>
+      {displayAction && <p>I am writting JSX</p>}
+      <ul>
+        {
+          emojis.map(emoji => (
+            <li key={emoji.name}>
+              <button onClick={displayEmojiName}>
+                <span role="img" aria-label={emoji.name} id={emoji.name}>{emoji.emoji}</span>
+              </button>
+            </li>
+          ))  // end of map
+        }
+      </ul>
     </div>
   );
 }
