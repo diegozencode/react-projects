55bb88d985260ab274a2799cb25c8f7fbe542780
Diego <difemo91@gmail.com>
2021-09-07 20:00:56 -0500

    [FEAT - COMPONENT] add my first class component

---
+++ digital-ocean/component-tutorial/src/App.js
@@ -0,0 +1,44 @@
+import Instructions from './Instructions';
+import './App.css';
+
+const displayEmojiName = event => alert(event.target.id);
+// Aray of objects
+const emojis = [
+  {
+    emoji: "😀",
+    name: "grinning face",
+  },
+  {
+    emoji: "🎉",
+    name: "party popper",
+  },
+  {
+    emoji: "💃",
+    name: "woman dancing",
+  },
+];
+
+function App() {
+  const greeting = "greeting";
+  const displayAction = true;
+  return (
+    <div className="container">
+      <h1 id={greeting}>Hello world</h1>
+      {displayAction && <p>I am writting JSX</p>}
+      <Instructions />
+      <ul>
+        {
+          emojis.map(emoji => (
+            <li key={emoji.name}>
+              <button onClick={displayEmojiName}>
+                <span role="img" aria-label={emoji.name} id={emoji.name}>{emoji.emoji}</span>
+              </button>
+            </li>
+          ))  // end of map
+        }
+      </ul>
+    </div>
+  );
+}
+
+export default App;
